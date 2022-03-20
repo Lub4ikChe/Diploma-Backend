8f3a09b7016654b85004f5f59ad4dc09700815cd
Liubomyr Chereshchuk <liubomyrchereshchuk@gmail.com>
2022-03-20 14:40:55 +0200

    fix: add temporary tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmCliConfigAsync } from './config/typeorm.config';
 
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -10,5 +12,7 @@ import { typeOrmCliConfigAsync } from './config/typeorm.config';
     }),
     TypeOrmModule.forRootAsync(typeOrmCliConfigAsync),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
